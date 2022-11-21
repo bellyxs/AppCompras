25953b2f6c1930b9cf415064d2698b07b900c10e
Usuario <Usuario@CPD02-M14>
2022-11-21 09:33:46 -0300

    Adicionar arquivos de projeto.

---
+++ AppCompras/AppCompras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using System.Threading.Tasks;
+using AppCompras.Model;
+using System.Globalization;
+
+namespace AppCompras.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+
+
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public void Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao = ?, Quantidade = ?, Preco = ? WHERE id = ?";
+
+            _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+
+        }
+
+        /*
+         * *
+        public Task<Produto> getById(int id)
+        {
+            return new Produto();
+        }
+        * *
+        */
+
+        public Task<List<Produto>> getAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE  '%" + q + "%' "; 
+
+            return _conn.QueryAsync<Produto>(sql);
+        }
+    }
+}
---
+++ AppCompras/AppCompras/View/Listagem.xaml.cs
@@ -0,0 +1,107 @@
+﻿using AppCompras.Model;
+using AppCompras.View;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCompras.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listagem : ContentPage
+    {
+        ObservableCollection<Produto> lista_produtos = new ObservableCollection<Produto>();
+
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked_Novo(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new NovoProduto());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Opa", ex.Message, "OK");
+            }
+
+        }
+
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
+        {
+            double soma = lista_produtos.Sum(i => i.Preco * i.Quantidade);
+
+            string msg = "O total da compra é: " + soma;
+
+            DisplayAlert("Opa", msg, "OK");
+        }
+
+        protected override void OnAppearing()
+        {
+            if(lista_produtos.Count == 0)   
+           
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                List<Produto> temp = await App.Database.getAll();
+
+                foreach (Produto item in temp)
+                {
+                    lista_produtos.Add(item);
+                }
+
+                ref_load.IsRefreshing = false;
+            });
+
+            lst_produtos.ItemsSource = lista_produtos;
+        }
+
+        private async void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            MenuItem disparador = (MenuItem)sender;
+            Produto produto_selecionado = (Produto)disparador.BindingContext;
+
+            bool confirmacao = await DisplayAlert("Tem Certeza?", "Remover Item?", "Sim", "Não");
+
+            if (confirmacao)
+            {
+                await App.Database.Delete(produto_selecionado.Id);
+                lista_produtos.Remove(produto_selecionado);
+
+            }
+        }
+
+        private void txt_busca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string busca = e.NewTextValue;
+
+            System.Threading.Tasks.Task.Run(async () =>
+            {
+                List<Produto> temp = await App.Database.Search(busca);
+
+                foreach (Produto item in temp)
+                {
+                    lista_produtos.Add(item);
+                }
+
+                ref_load.IsRefreshing = false;
+            });
+        }
+
+        private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            Navigation.PushAsync(new EditarProduto
+            {
+                BindingContext = (Produto)e.SelectedItem
+            });                       
+        }
+    }
+}
